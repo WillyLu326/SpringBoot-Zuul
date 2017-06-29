491d7c0167e7c40d6609451156224424cb13af78
WillyLu326 <luzh0326@gmail.com>
2017-06-28 22:18:36 -0400

    add preFilter

---
+++ api-gateway/src/main/java/com/example/filters/PreFilter.java
@@ -0,0 +1,39 @@
+package com.example.filters;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+public class PreFilter extends ZuulFilter {
+
+	private static Logger log = LoggerFactory.getLogger(PreFilter.class);
+	
+	@Override
+	public Object run() {
+		// TODO Auto-generated method stub
+		RequestContext ctx = RequestContext.getCurrentContext();
+		HttpServletRequest request = ctx.getRequest();
+		log.info(String.format("%s request to %s", request.getMethod(), request.getRequestURL()));
+		return null;
+	}
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public int filterOrder() {
+		return 1;
+	}
+
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+
+}
